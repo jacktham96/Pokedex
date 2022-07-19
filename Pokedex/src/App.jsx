3e9d0426d460ee56fdab1fdf83b827233818a4bf
jacktham96 <62689233+jacktham96@users.noreply.github.com>
2022-07-19 23:15:41 +0800

    Fetch Data success

--- Pokedex/src/App.jsx
+++ Pokedex/src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
 
 function App() {
+
+  const [pokemon , setPokemon] = useState([])
+  const [nextUrl , setNextUrl] = useState('')
+  const [prevUrl , setPrevUrl] = useState('')
+  const [loading , setLoading] = useState(true)
+
+  async function getPokemonData(url) {
+      return new Promise((resolve, reject) => {
+          fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            resolve(data)
+          })
+      })
+    }
+  
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getPokemonData('https://pokeapi.co/api/v2/pokemon?limit=20')
+      console.log(data);
+      setNextUrl(data.next)
+      setPrevUrl(data.previous)
+      setPokemon(data.results)
+      setLoading(false)
+    }
+    fetchData()
+  } ,[])
+
+
+
+
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Pokedex</h1>
+      { loading ? <h1>Loading...</h1>
+        :(<h1>Data is Fetched</h1>)
+      }
     </div>
   )
 }
